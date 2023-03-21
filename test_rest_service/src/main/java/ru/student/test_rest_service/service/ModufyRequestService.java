2a57c69dc4841b2dcec1c38dd750044757c953ae
Skeptik96 <97885603+Skeptik96@users.noreply.github.com>
2023-03-21 09:51:31 +0500

    Add files via upload

---
+++ test_rest_service/src/main/java/ru/student/test_rest_service/service/ModufyRequestService.java
@@ -0,0 +1,6 @@
+package ru.student.test_rest_service.service;
+
+import ru.student.test_rest_service.model.Request;
+public interface ModufyRequestService {
+    void modifyRq(Request request);
+}
---
+++ test_rest_service/src/main/java/ru/student/test_rest_service/service/MyModifyService.java
@@ -0,0 +1,9 @@
+package ru.student.test_rest_service.service;
+
+import ru.student.test_rest_service.model.Response;
+
+public interface MyModifyService {
+
+    Response modify(Response response);
+
+}
